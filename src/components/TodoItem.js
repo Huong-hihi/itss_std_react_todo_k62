33fc958f404d685a7b501b59c000380768404599
Huong-hihi <dothihuong030499@gmail.com>
2021-04-12 04:06:06 +0000

    Change Todo status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,11 +4,23 @@
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem(  ) {
+const handleClickChecBox = (key) => {
+    const element = document.getElementById(key);
+    if(element.style.color === "grey"){
+      element.style.color = 'black';
+    }else{
+      element.style.color = "grey";
+    }
+};
+
+function TodoItem({item}) {
   return (
     <label className="panel-block">
+            <input type="checkbox" />
+    <label className="panel-block" id={item.key}>
+            <input type="checkbox" onClick={() => handleClickChecBox(item.key)}/>
+            {item.text}
+        </label>
     </label>
   );
 }
-
-export default TodoItem;
